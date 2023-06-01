28914709c9028a94d3389e942accf2bc58462d15
RAHULGUPTADATASCIENTIST <rg48bbd@gmail.com>
2023-06-02 05:22:41 +0530

    modification

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = sys.exc_info()
@@ -16,10 +17,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except ZeroDivisionError as e:
-        error_message = "Divide by zero: {0}".format(str(e))
-        logging.error(error_message)
-        raise CustomException(error_message, sys)
+
